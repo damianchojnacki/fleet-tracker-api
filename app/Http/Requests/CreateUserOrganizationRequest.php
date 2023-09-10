ec83acdb4ba0bd82fb187620f4449cbc270210fe
Damian Chojnacki <damianchojnacki@op.pl>
2023-09-10 12:07:08 +0200

    feature: unique organization name

--- app/Http/Requests/CreateUserOrganizationRequest.php
+++ app/Http/Requests/CreateUserOrganizationRequest.php
@@ -25,7 +25,7 @@ class CreateUserOrganizationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:organizations,name'],
         ];
     }
 }
